9756cc1c2e51c6ff5a7b6219e2237fba88e18fe3
cichowlasp <cichowlas.p@gmail.com>
2024-08-15 21:05:00 +0200

    added some logic behind copy button and added first section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,24 @@
+import { transform } from 'typescript';
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+				wiggle: {
+					'0%, 100%': {
+						transform: 'translate(150px, -150px) rotate(-12deg)',
+					},
+					'50%': {
+						transform: 'translate(150px, -130px) rotate(-12deg)',
+					},
+				},
+			},
+			animation: {
+				wiggle: 'wiggle 3s ease-in-out infinite',
+			},
+		},
 	},
 	plugins: [],
 };
