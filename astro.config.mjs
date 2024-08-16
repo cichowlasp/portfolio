09adfc12ac1b8e8059e02db589c3bd9dfbc4a8c5
cichowlasp <cichowlas.p@gmail.com>
2024-08-16 12:02:34 +0200

    added mobile adjustments

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react(), tailwind()],
-	site: 'https://cichowlasp.github.io',
-	base: '/portfolio',
+  integrations: [react(), tailwind()],
+  site: 'https://cichowlasp.github.io',
+  base: '/portfolio'
 });
