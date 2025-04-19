1122815a3fa40c3497925e33c8d3097150431a28
cichowlasp <cichowlas.p@gmail.com>
2025-04-19 15:37:44 +0200

    added seo things

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,20 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react(), tailwind()],
+	integrations: [
+		react(),
+		tailwind(),
+		sitemap({
+			// You can customize the sitemap options here
+			changefreq: 'weekly',
+			priority: 0.7,
+			lastmod: new Date(),
+		}),
+	],
 	site: 'https://portfolio.cichowlasp.org',
 	base: '/',
 });
